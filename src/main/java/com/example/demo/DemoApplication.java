f75ecbfce1257c2fc50a563c9ae93fbc30c5acba
Your Name <you@example.com>
2023-09-07 21:46:59 +0530

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class DemoApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("hellow world");
+	}
+
+}
